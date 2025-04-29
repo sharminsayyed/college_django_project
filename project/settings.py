79cf6425e10bd7d447f407962042f1e00d8ba997
Sharmin Sayyed <116384332+sharminsayyed@users.noreply.github.com>
2025-04-29 16:49:15 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # your source static folder
